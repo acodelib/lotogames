ff28000370be4a6e90eee10b9e1942b118500674
Andrei Gurguta <andrei_gurguta@yahoo.com>
2019-12-18 00:32:21 +0200

    GameCore shape advance

--- src/lotogames/GameCore.java
+++ src/lotogames/GameCore.java
@@ -1,5 +1,25 @@
 package lotogames;
 
 public class GameCore {
-
+	public GameCore() {
+		//read configs
+		//set attributes
+	}
+	
+	public static void main (String...args) {
+		//instantiate game core
+		//start GameSessions thread
+		//start Mailer thread
+	}
+	
+	public void runGameSessions() {
+		//start game thread
+	}
+	
+	public void runMailer() {
+		//start thread with mail listener and communicator
+	}
+	
+	
+	
 }

e23871e8ee4a85ed2f44687862a90316508a5146
Andrei Gurguta <andrei_gurguta@yahoo.com>
2020-01-05 15:53:45 +0200

    First attempt of UPDATE and RELOAD 649 archive

--- src/lotogames/DbOps.java
+++ src/lotogames/DbOps.java
@@ -1,6 +1,7 @@
 package lotogames;
 
 import java.beans.Statement;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,6 +11,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.Iterator;
 import java.util.Map;
 import java.sql.DatabaseMetaData;
+import java.util.ArrayList;
+
 
 public class DbOps {
 	Connection conn;
@@ -22,12 +25,11 @@ public class DbOps {
 		
 		int a = 1;
 		while (a <2) {
-			thisgame = new Loto649Session();
-			System.out.println(a);
+			thisgame = new Loto649Session();			
 			int i = 1;		
 			int ix =  loto_db.getNewIx("LotoGame");			
 			for (GameResult r: thisgame.playGame(8, true)) {			
-				//loto_db.saveGameResult(r, ix,i);				
+				loto_db.saveGameResult(r, ix,i);				
 				System.out.println(r.gameToStringSimple().trim());
 				i++;
 			}			
@@ -64,9 +66,9 @@ public class DbOps {
 			ins.setString(5, (String)pair.getKey());
 			ins.setInt(6, (int)pair.getValue());
 			ins.execute();
-		}
-		
+		}		
 	}
+	
 	private int getNewIx(String optional_text) throws SQLException {
 		//get new ID from table GAME CORE KEYS
 		
@@ -88,4 +90,76 @@ public class DbOps {
 		
 		return ix;				
 	}
+	
+	public void insertArchive649NewLines(ArchiveFetcher get_new_lines) throws SQLException, IOException {
+		//gets max of id in game_loto649_draws. then checks the web if any new ones were played. if so fetch them in
+		
+		int last_ck 	  			= 0;				
+		String sql_insrt  			= "INSERT INTO game_loto649_draws (date,n1,n2,n3,n4,n5,n6) VALUES (?,?,?,?,?,?,?)";
+		PreparedStatement ins		= this.conn.prepareStatement(sql_insrt);
+		String sql_max    			= "Select max(id) FROM game_loto649_draws";
+		java.sql.Statement get_ck   = this.conn.createStatement();
+		ArrayList<String[]> lines 	= new ArrayList<String[]>();
+		
+			
+		//get last inserted id from table with loto history		
+		ResultSet rs_ck = get_ck.executeQuery(sql_max);
+		while(rs_ck.next())
+			last_ck = rs_ck.getInt(1);
+		
+						
+		//get results from the web
+		lines = get_new_lines.fetchNewLoto649(last_ck);
+		if (lines.size() == 0) {
+			System.out.println("@insertAcrchive649NewLines --> No new games to insert");			
+			return;
+		}			
+		for(String[] line: lines) {
+			for (int i = 0; i< line.length;i++) {
+				ins.setString(i+1, line[i]);				
+			}
+			ins.execute();
+		}
+		this.conn.commit();
+	}
+
+	public void refillArchive649(ArchiveFetcher get_arch) throws SQLException, IOException {
+		//clears (***recreate) game_loto649_draws and fetches all archive	
+			
+		
+		String sql_insrt  			= "INSERT INTO game_loto649_draws (date,n1,n2,n3,n4,n5,n6) VALUES (?,?,?,?,?,?,?)";
+		PreparedStatement ins		= this.conn.prepareStatement(sql_insrt);
+		String sql_clear   			= 	"DROP TABLE IF EXISTS game_loto649_draws;\r\n" + 
+										"CREATE TABLE game_loto649_draws (\r\n" + 
+										"	id INTEGER PRIMARY KEY,\r\n" + 
+										"	date text,\r\n" + 
+										"	n1 INTEGER,\r\n" + 
+										"	n2 INTEGER,\r\n" + 
+										"	n3 INTEGER,\r\n" + 
+										"	n4 INTEGER,\r\n" + 
+										"	n5 INTEGER,\r\n" + 
+										"	n6 INTEGER\r\n" + 
+										");";		
+		//java.sql.Statement clear 	= this.conn.createStatement();
+		PreparedStatement clear 	= this.conn.prepareStatement(sql_clear);
+		ArrayList<String[]> lines 	= new ArrayList<String[]>();
+		
+			
+		//get last inserted id from table with loto history		
+		clear.execute();
+		
+		
+						
+		//get results from the web
+		lines = get_arch.fetchNewLoto649(0);					
+		for(String[] line: lines) {
+			for (int i = 0; i< line.length;i++) {
+				ins.setString(i+1, line[i]);				
+			}
+			ins.execute();
+		}
+		this.conn.commit();
+	}
+	
 }
+
--- src/lotogames/GameCore.java
+++ src/lotogames/GameCore.java
@@ -1,15 +1,23 @@
 package lotogames;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+
 public class GameCore {
 	public GameCore() {
 		//read configs
 		//set attributes
 	}
 	
-	public static void main (String...args) {
+	public static void main (String...args) throws SQLException, IOException {
 		//instantiate game core
 		//start GameSessions thread
 		//start Mailer thread
+		
+		GameCore the_core = new GameCore();
+		//the_core.importResultsFromWebArchive649();
+		the_core.refillArchive649();
 	}
 	
 	public void runGameSessions() {
@@ -18,6 +26,20 @@ public class GameCore {
 	
 	public void runMailer() {
 		//start thread with mail listener and communicator
+	}
+	
+	public void importResultsFromWebArchive649() throws SQLException, IOException {
+		DbOps db = new DbOps();
+		ArchiveFetcher af = new ArchiveFetcher();
+		
+		db.insertArchive649NewLines(af);
+	}
+	
+	public void refillArchive649() throws SQLException, IOException {
+		DbOps db = new DbOps();
+		ArchiveFetcher af = new ArchiveFetcher();
+		
+		db.refillArchive649(af);
 	}
 	
 	
